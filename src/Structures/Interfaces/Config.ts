096bd6d604be597f1599710a949354e99985fedd
Willow Snow <willow.snow14@gmail.com>
2020-07-10 13:44:54 -0500

    Remove RoleMention from config interface

--- src/Structures/Interfaces/Config.ts
+++ src/Structures/Interfaces/Config.ts
@@ -1,8 +1,6 @@
 import { VoiceChannelHelperConfig } from "../../Modules/";
-import { RoleMentionConfig } from "../../Modules/";
 
 export interface Config {
     token: string;
     VoiceChannelHelper: VoiceChannelHelperConfig;
-    RoleMention: RoleMentionConfig;
 }
