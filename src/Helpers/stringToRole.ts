e8b47dbd62ce01011ebcb1d226a5eaa9c1c4d49f
Willow Snow <willow.snow14@gmail.com>
2020-07-10 13:22:58 -0500

    Remove RoleMention module

--- src/Helpers/stringToRole.ts
+++
@@ -1,9 +0,0 @@
-import { Message, Role, Guild, Channel } from "discord.js";
-
-export function messageStringToRole(message: Message, role: string): Role {
-    return message.guild.roles.find((prop) => prop.name.toLowerCase() == role.toLowerCase()) || message.guild.roles.find((prop) => prop.id == role);
-}
-
-export function guildStringToRole(guild: Guild, role: string): Role {
-    return guild.roles.find((prop) => prop.name === role) || guild.roles.get(role);
-}
